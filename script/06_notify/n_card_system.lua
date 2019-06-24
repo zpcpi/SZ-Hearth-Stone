66acf00dc06ab6d7bfb045e9d8b1add8c584edc2
zpc.pi <406553338@qq.com>
2019-06-25 01:25:05 +0800

    预览布局调整

--- script/06_notify/n_card_system.lua
+++ script/06_notify/n_card_system.lua
@@ -7,32 +7,40 @@ function noti.card_create()
     ui_obj = G.getUI('v_card_minion')
     if not ui_obj then
         ui_obj = G.addUI('v_card_minion')
-        ui_obj.x = ui_obj.x - 450
-        ui_obj.y = ui_obj.y - 100
+        ui_obj.x = ui_obj.x - 500
+        ui_obj.y = ui_obj.y + 200
+        ui_obj.scaleX = 0.5
+        ui_obj.scaleY = 0.5
     end
 
     -- 法术预览
     ui_obj = G.getUI('v_card_spell')
     if not ui_obj then
         ui_obj = G.addUI('v_card_spell')
-        ui_obj.x = ui_obj.x - 150
-        ui_obj.y = ui_obj.y + 100
+        ui_obj.x = ui_obj.x - 300
+        ui_obj.y = ui_obj.y + 200
+        ui_obj.scaleX = 0.5
+        ui_obj.scaleY = 0.5
     end
 
     -- 英雄预览
     ui_obj = G.getUI('v_card_hero')
     if not ui_obj then
         ui_obj = G.addUI('v_card_hero')
-        ui_obj.x = ui_obj.x + 150
-        ui_obj.y = ui_obj.y - 100
+        ui_obj.x = ui_obj.x - 100
+        ui_obj.y = ui_obj.y + 200
+        ui_obj.scaleX = 0.5
+        ui_obj.scaleY = 0.5
     end
 
     -- 武器预览
     ui_obj = G.getUI('v_card_weapon')
     if not ui_obj then
         ui_obj = G.addUI('v_card_weapon')
-        ui_obj.x = ui_obj.x + 450
-        ui_obj.y = ui_obj.y + 100
+        ui_obj.x = ui_obj.x + 100
+        ui_obj.y = ui_obj.y + 200
+        ui_obj.scaleX = 0.5
+        ui_obj.scaleY = 0.5
     end
 
 end
