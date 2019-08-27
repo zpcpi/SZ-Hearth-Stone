a3cb636f0119982fe1913133e56363f88f0f0496
zpc.pi <406553338@qq.com>
2019-08-28 01:51:40 +0800

    farg接口扩充

--- script/04_program/p_farg_system.lua
+++ script/04_program/p_farg_system.lua
@@ -9,19 +9,19 @@ local t = G.api
 --type=farg
 t['farg_block'] = function(_farg_actions)
     local result = nil
-    for _,v in ipairs(_farg_actions) do
-        result = G.call(v)
+    for _,f in ipairs(_farg_actions) do
+        result = G.call(f)
     end
     return result
 end
 
 --hide=true
 --type=farg
-t['farg_if'] = function(fargboolean_condition, _farg_ifaction, _farg_elseaction)
+t['farg_if'] = function(fargboolean_condition, _farg_ifactions, _farg_elseactions)
     if G.call(fargboolean_condition) then
-        return G.call('farg_block', _farg_ifaction)
+        return G.call('farg_block', _farg_ifactions)
     else
-        return G.call('farg_block', _farg_elseaction)
+        return G.call('farg_block', _farg_elseactions)
     end
 end
 
@@ -33,6 +33,38 @@ t['farg_while'] = function(fargboolean_condition, _farg_actions)
         result = G.call('farg_block', _farg_actions)
     end
     return result
+end
+
+--hide=true
+--type=farg
+--ret=boolean
+t['fargboolean_and'] = function(_fargboolean_conditions)
+    for _,c in ipairs(_fargboolean_conditions) do
+        if G.call(c) then
+        else
+            return false
+        end
+    end
+    return true
+end
+
+--hide=true
+--type=farg
+--ret=boolean
+t['fargboolean_or'] = function(_fargboolean_conditions)
+    for _,c in ipairs(_fargboolean_conditions) do
+        if G.call(c) then
+            return true
+        end
+    end
+    return false
+end
+
+--hide=true
+--type=farg
+--ret=boolean
+t['fargboolean_not'] = function(fargboolean_condition)
+    return not G.call(fargboolean_condition)
 end
 
 
