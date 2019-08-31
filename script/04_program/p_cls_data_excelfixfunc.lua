d387a5f8b1c114bc20c5a35eef1b460eb4351c7b
zpc.pi <406553338@qq.com>
2019-08-31 15:49:44 +0800

    批量修正卡牌原画位置

--- script/04_program/p_cls_data_excelfixfunc.lua
+++ script/04_program/p_cls_data_excelfixfunc.lua
@@ -7,6 +7,16 @@ local t = G.api
 
 --type=excel_fix
 t['o_card_卡牌信息选择导入'] = function(_o_card_卡牌列表)
+    for k,v in ipairs(_o_card_卡牌列表) do
+        if v.类型 == 0x10090005 then
+            -- 法术卡
+            v['原画缩放'] = v['原画缩放'] or 0.9
+            v['原画偏移Y'] = v['原画偏移Y'] or -14
+        elseif v.类型 == 0x10090004 then
+            -- 随从卡
+            v['原画偏移Y'] = v['原画偏移Y'] or -6
+        end
+    end
 
     return _o_card_卡牌列表
 end
