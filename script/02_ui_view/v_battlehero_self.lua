63c4b0ddfc27782435d538f1298850553c5a98b0
zpc.pi <406553338@qq.com>
2019-10-06 12:37:21 +0800

    战场英雄界面控件添加

--- script/02_ui_view/v_battlehero_self.lua
+++ script/02_ui_view/v_battlehero_self.lua
@@ -2,6 +2,8 @@
 
 ]]
 local G = require 'gf'
+local c_battle_weapon = require 'c_battle_weapon'
+local c_battle_herospell = require 'c_battle_herospell'
 local c_mintextquadsize = require 'c_mintextquadsize'
 local c_card_hero = require 'c_card_hero'
 local tp,tc,com,tk
@@ -42,6 +44,34 @@ tp = tc
 		tc.top = 0.000
 		tc.scaleX = 0.280
 		tc.scaleY = 0.280
+		tp = tc
+		tp = tp.parent
+		--end
+		tc = G.loadUI('v_battle_herospell')
+		tc.prefab = true
+		tp.addChild(tc)
+		tc.name = '英雄技能'
+		tc.left = -150.000
+		tc.right = 150.000
+		tc.bottom = -150.000
+		tc.top = 150.000
+		tc.anchor = 0x32325050
+		tc.scaleX = 0.350
+		tc.scaleY = 0.350
+		tp = tc
+		tp = tp.parent
+		--end
+		tc = G.loadUI('v_battle_weapon')
+		tc.prefab = true
+		tp.addChild(tc)
+		tc.name = '武器'
+		tc.left = -150.000
+		tc.right = 150.000
+		tc.bottom = -150.000
+		tc.top = 150.000
+		tc.anchor = 0x32321414
+		tc.scaleX = 0.300
+		tc.scaleY = 0.300
 		tp = tc
 		tp = tp.parent
 		--end
