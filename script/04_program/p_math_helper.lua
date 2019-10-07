3351ea0b44d62d47292d5a507308f13e2ea62578
zpc.pi <406553338@qq.com>
2019-10-08 03:30:58 +0800

    随机库功能规划

--- script/04_program/p_math_helper.lua
+++ script/04_program/p_math_helper.lua
@@ -5,26 +5,27 @@ local G = require "gf"
 local L = {}
 local t = G.api
 
+-- 组合数值计算
 local Combination_Number_list = {}
 --hide=true
 --type=math
-t['Combination_Number'] = function (n, m)
-    if (m >= 0) and (n <= m) and (n >= 0) then
-        local line_m = Combination_Number_list[m]
+t['Combination_Number'] = function (count, max)
+    if (max >= 0) and (count <= max) and (count >= 0) then
+        local line_m = Combination_Number_list[max]
 
         if line_m then
         else
             line_m = {}
             line_m[0] = 1
-            line_m[m] = 1
-            Combination_Number_list[m] = line_m
+            line_m[max] = 1
+            Combination_Number_list[max] = line_m
         end
 
-        if line_m[n] then
-            return line_m[n]
+        if line_m[count] then
+            return line_m[count]
         else
-            local result = G.call('Combination_Number', n - 1, m - 1) + G.call('Combination_Number', n, m - 1)
-            line_m[n] = result
+            local result = G.call('Combination_Number', count - 1, max - 1) + G.call('Combination_Number', count, max - 1)
+            line_m[count] = result
             return result
         end
     else
@@ -32,6 +33,7 @@ t['Combination_Number'] = function (n, m)
     end
 end
 
+-- 贝赛尔曲线相关
 t['Bezier_curve'] = function (posxs, posys, dots)
     local pointxs, pointys = {}, {}
 
@@ -78,3 +80,75 @@ t['Bezier_curve_bycount'] = function (posxs, posys, count, dotpro)
         return G.call('Bezier_curve', posxs, posys, dots)
     end
 end
+
+-- 随机库功能
+t['Create_Randomlib'] = function (estr_randomlib_type_随机库类型)
+    local rlib = {}
+
+    -- 数据
+    rlib.datas = {}
+    -- {value, weight, cur_weight, condition}
+    
+    -- 属性
+    rlib.boolean_是否已初始化 = false
+    rlib.int_概率基底 = 1
+    rlib.boolean_是否循环 = false
+    rlib.max_count = 0
+
+    -- 添加数据 {数值, 权重, 条件} 编号
+    -- 修改数据 类型 编号
+    -- 删除数据 编号
+    -- 设置概率基底 数值
+    -- 设置循环 数值
+
+    -- 查看数据 类型
+    
+    -- 初始化 (是否计算条件, 是否重置权重)
+
+    -- 求值 次数
+
+
+    rlib['添加数据'] = function (data, index)
+        table.insert(rlib.datas, data, index)
+
+        rlib.boolean_是否已初始化 = false
+    end
+
+    rlib['修改数据'] = function (data_type, index, value)
+        local data = rlib.datas[index]
+        if data and (type(data) == 'table') then
+            data[data_type] = value
+
+            rlib.boolean_是否已初始化 = false
+        end
+    end
+
+    rlib['删除数据'] = function (index)
+        table.remove(rlib.datas, index)
+
+        rlib.boolean_是否已初始化 = false
+    end
+
+    rlib['设置概率基底'] = function (base)
+        if base > 0 then
+            rlib.int_概率基底 = base
+        end
+    end
+
+    rlib['设置循环'] = function (loop)
+        rlib.boolean_是否循环 = loop
+    end
+
+    rlib['查看数据库数据'] = function (data_type)
+
+    end
+
+    -- 求值用元表
+
+end
+
+
+
+
+
+
--- script/type/estrdream.lua
+++ script/type/estrdream.lua
@@ -4,6 +4,10 @@ if type(t) ~= 'table' then t = {} end
 t.estr_player = {'我方', '友方1', '敌方1', '敌方2'}
 t.estr_absolute_id_type = {'红1', '红2', '蓝1', '蓝2'}
 t.estr_battle_type = {'1v1', '2v2'}
+t.estr_randomlib_type = {'完全随机', '抽取随机', '有损随机', '顺序选取'}
+
+
+
 t.estr_ui_layer_type = {'一级界面', '二级界面'}
 
 t.estr_ui_battlehero_type = {'英雄', '英雄技能', '武器'}
