132cb15f58c5e9f3e11b1d48c1e3495e3d47cf8d
gemfunc <406553338@qq.com>
2019-12-20 16:24:07 +0800

    随从攻击指令流程添加

--- script/04_program/p_order.lua
+++ script/04_program/p_order.lua
@@ -102,9 +102,16 @@ t['卡牌注册指令'] = function (o_card_使用卡牌, i_order_当前指令)
                 -- 执行卡牌使用效果
                 G.call('卡牌使用_主流程', '我方', o_order_info_当前指令信息)
             elseif state == -2 then
+                -- 执行失败
                 G.call('卡牌注册指令_退出', o_order_info_当前指令信息)
+
                 -- 重新注册指令
                 G.call('卡牌注册指令', o_card_使用卡牌, i_order_当前指令)
+            elseif state == -3 then
+                -- 攻击指令完成
+                G.call('卡牌注册指令_完成', o_order_info_当前指令信息)
+
+                -- 执行卡牌攻击效果
             end
         end
 
