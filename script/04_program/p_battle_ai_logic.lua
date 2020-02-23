04fec6a0c1cbec01a855a3c04ecb2279e79c93cf
alanchen <alanchen@dianhun.cn>
2020-02-23 17:24:09 +0800

    增加AI逻辑

--- script/04_program/p_battle_ai_logic.lua
+++ script/04_program/p_battle_ai_logic.lua
@@ -22,21 +22,24 @@ t['AI逻辑_弱智酒馆老板_回合开始'] = function()
     else
         G.call('提示_添加提示', '哒哒哒!')
     end
+    -- 只要有卡能用就一直用
     while G.call('角色_是否剩余可用水晶', estr_absolute_id_type_绝对身份) do 
         local _o_card_可用卡牌 = G.call('角色_获取可用卡牌', estr_absolute_id_type_绝对身份, true)
         if #_o_card_可用卡牌 == 0 then
             break 
         end
+        -- 使用第一张可用卡牌
         local o_card_使用卡牌 = _o_card_可用卡牌[1]
         local estr_absolute_id_type_敌方绝对身份 = G.call('对决_获取上一个行动角色绝对身份', estr_absolute_id_type_绝对身份)
+        -- 目标是敌方英雄, 直接打脸
         local o_card_敌方英雄 = G.call('角色_战场_获取英雄_绝对身份', estr_absolute_id_type_敌方绝对身份)
-        G.call('卡牌使用_主流程', estr_player_相对身份, )
-        -- TODO: 获取对方英雄
-        -- 使用第一张可用卡牌
+        local o_order_info_当前指令信息 = {
+            Caster = o_card_使用卡牌,
+            Target = {o_card_敌方英雄},
+            Player = estr_player_相对身份
+        }
+        G.call('卡牌使用_主流程', estr_player_相对身份, o_order_info_当前指令信息)
     end
-    -- TODO: 循环, 直到没有可用卡牌和技能
-    -- TODO: 获取可用卡牌
-    -- TODO: 使用可用卡牌
     G.call('对决_回合结束', estr_absolute_id_type_绝对身份)
 end
 
