8fad1d9ca0e886db32a1fb16e54ef13a2d1c077d
gemfunc <406553338@qq.com>
2020-04-15 01:05:30 +0800

    辅助函数中增加一个比较函数

--- script/type/estrdream.lua
+++ script/type/estrdream.lua
@@ -37,4 +37,6 @@ t.estr_battleinfo_type = {'伤害', '治疗'}
 
 t.estr_ui_component = {'战场随从', '战场英雄', '手牌', '牌库', '历史记录', '执行队列', '水晶'}
 
+t.estr_compare_op = {'==', '~=', '>=', '<=', '>', '<'}
+
 return t
