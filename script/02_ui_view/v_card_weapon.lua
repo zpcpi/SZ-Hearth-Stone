8539b29c343d0e0e3e6ae7cf4414404be94089d2
gemfunc <406553338@qq.com>
2020-05-24 19:44:41 +0800

    卡牌武器UI更新

--- script/02_ui_view/v_card_weapon.lua
+++ script/02_ui_view/v_card_weapon.lua
@@ -2,8 +2,8 @@
 
 ]]
 local G = require 'gf'
-local c_curved_text = require 'c_curved_text'
 local c_card_weapon = require 'c_card_weapon'
+local c_curved_text = require 'c_curved_text'
 local c_mintextquadsize = require 'c_mintextquadsize'
 local tp,tc,com,tk
 tc = G.Entity()
@@ -34,6 +34,8 @@ tp = tc
 			tc.right = 182.500
 			tc.bottom = -256.000
 			tc.top = 256.000
+			tc.scaleX = 1.100
+			tc.scaleY = 1.100
 			tc.mask = 10
 			tc.shapeimg = 0x560a0011
 			tc.SetVertexBuf(37,{0.0,0.0,-111.0,110.0,-112.0,110.0,-110.0,79.0,-109.0,79.0,-105.0,60.0,-90.0,33.0,-72.0,13.0,-47.0,-3.0,-47.0,-4.0,-32.0,-9.0,-6.0,-14.0,11.0,-14.0,30.0,-11.0,55.0,-1.0,80.0,17.0,89.0,28.0,103.0,51.0,112.0,78.0,114.0,104.0,113.0,105.0,113.0,112.0,106.0,138.0,95.0,160.0,86.0,172.0,68.0,189.0,61.0,194.0,38.0,205.0,24.0,209.0,-6.0,212.0,-36.0,206.0,-59.0,195.0,-77.0,181.0,-84.0,174.0,-100.0,151.0,-109.0,128.0,-111.0,110.0}, {0,0},0xffffffff)
@@ -49,9 +51,20 @@ tp = tc
 			tc.bottom = -256.000
 			tc.top = 256.000
 			tc.anchor = 0x46463232
-			tc.scaleX = 0.480
-			tc.scaleY = 0.480
+			tc.scaleX = 0.530
+			tc.scaleY = 0.530
 			tc.img = 0x56010009
+			tp = tc
+			tp = tp.parent
+			--end
+			tc = G.Quad()
+			tp.addChild(tc)
+			tc.name = '插画边缘阴影'
+			tc.left = -117.500
+			tc.right = 117.500
+			tc.bottom = -10.250
+			tc.top = 227.750
+			tc.img = 0x560a0005
 			tp = tc
 			tp = tp.parent
 			--end
@@ -60,14 +73,72 @@ tp = tc
 		tc = G.Quad()
 		tp.addChild(tc)
 		tc.name = '背景'
-		tc.left = -250.000
-		tc.right = 250.000
-		tc.bottom = -353.500
-		tc.top = 353.500
-		tc.scaleX = 0.900
-		tc.scaleY = 0.900
-		tc.img = 0x560a000c
+		tc.left = -173.000
+		tc.right = 173.000
+		tc.bottom = -252.500
+		tc.top = 252.500
+		tc.img = 0x560a0001
 		tp = tc
+		tp = tp.parent
+		--end
+		tc = G.Quad()
+		tp.addChild(tc)
+		tc.name = '名称板'
+		tc.left = -166.500
+		tc.right = 166.500
+		tc.bottom = -56.750
+		tc.top = 13.250
+		tc.img = 0x560a0003
+		tp = tc
+			tc = G.loadUI('v_curved_text')
+			tc.prefab = true
+			tp.addChild(tc)
+			tc.name = '名称字符'
+			tc.left = -320.000
+			tc.right = 320.000
+			tc.bottom = -4.000
+			tc.top = -4.000
+			tp = tc
+			tp.c_curved_text = setmetatable({}, c_curved_text)
+			com = tp.c_curved_text
+			com['text'] ='弑君'
+			com['p0x'] =-142.000
+			com['p0y'] =15.000
+			com['p1x'] =-104.000
+			com['p1y'] =15.000
+			com['p2x'] =70.000
+			com['p2y'] =15.000
+			com['p3x'] =133.000
+			com['p3y'] =15.000
+			com['space'] =10.000
+			tp = tp.parent
+			--end
+		tp = tp.parent
+		--end
+		tc = G.Quad()
+		tp.addChild(tc)
+		tc.name = '描述板'
+		tc.left = -154.500
+		tc.right = 146.500
+		tc.bottom = -233.500
+		tc.top = -59.500
+		tc.img = 0x560a0002
+		tp = tc
+			tc = G.TextQuad()
+			tp.addChild(tc)
+			tc.name = '描述字符'
+			tc.left = -140.000
+			tc.right = 140.000
+			tc.bottom = -72.000
+			tc.top = 72.000
+			tc.shadowAlpha = 150
+			tc.text = '          亡语：[br]将这把武器洗入你的[br]     牌库。保留所[br]     有额外效果。'
+			tc.font = 0x65170000
+			tc.lineSpace = 2
+			tc.outlineSize = 0.000
+			tp = tc
+			tp = tp.parent
+			--end
 		tp = tp.parent
 		--end
 		tc = G.Quad()
@@ -75,8 +146,10 @@ tp = tc
 		tc.name = '品质板'
 		tc.left = -25.000
 		tc.right = 29.000
-		tc.bottom = -45.000
-		tc.top = -29.000
+		tc.bottom = -54.000
+		tc.top = -38.000
+		tc.scaleX = 1.100
+		tc.scaleY = 1.100
 		tc.img = 0x560a0012
 		tp = tc
 			tc = G.Quad()
@@ -95,51 +168,11 @@ tp = tc
 		tc = G.Quad()
 		tp.addChild(tc)
 		tc.name = '精英板'
-		tc.left = -90.000
-		tc.right = 166.000
-		tc.bottom = 104.000
-		tc.top = 264.000
-		tc.scaleX = 0.900
-		tc.scaleY = 0.900
+		tc.left = -95.278
+		tc.right = 185.722
+		tc.bottom = 121.333
+		tc.top = 283.333
 		tc.img = 0x560a000f
-		tp = tc
-		tp = tp.parent
-		--end
-		tc = G.loadUI('v_curved_text')
-		tc.prefab = true
-		tp.addChild(tc)
-		tc.name = '名称字符'
-		tc.left = -320.000
-		tc.right = 320.000
-		tc.bottom = -24.000
-		tc.top = -24.000
-		tp = tc
-		tp.c_curved_text = setmetatable({}, c_curved_text)
-		com = tp.c_curved_text
-		com['text'] ='弑君'
-		com['p0x'] =-142.000
-		com['p0y'] =15.000
-		com['p1x'] =-104.000
-		com['p1y'] =15.000
-		com['p2x'] =70.000
-		com['p2y'] =15.000
-		com['p3x'] =133.000
-		com['p3y'] =15.000
-		com['space'] =10.000
-		tp = tp.parent
-		--end
-		tc = G.TextQuad()
-		tp.addChild(tc)
-		tc.name = '描述字符'
-		tc.left = -134.000
-		tc.right = 146.000
-		tc.bottom = -199.000
-		tc.top = -55.000
-		tc.shadowAlpha = 150
-		tc.text = '          亡语：[br]将这把武器洗入你的[br]     牌库。保留所[br]     有额外效果。'
-		tc.font = 0x65170000
-		tc.lineSpace = 2
-		tc.outlineSize = 0.000
 		tp = tc
 		tp = tp.parent
 		--end
@@ -153,12 +186,12 @@ tp = tc
 			tc = G.Quad()
 			tp.addChild(tc)
 			tc.name = '费用'
-			tc.left = -187.000
-			tc.right = -71.000
-			tc.bottom = 136.000
-			tc.top = 254.000
-			tc.scaleX = 0.750
-			tc.scaleY = 0.750
+			tc.left = -197.625
+			tc.right = -81.625
+			tc.bottom = 151.250
+			tc.top = 269.250
+			tc.scaleX = 0.800
+			tc.scaleY = 0.800
 			tc.img = 0x560b0001
 			tp = tc
 				tc = G.TextQuad()
@@ -173,6 +206,7 @@ tp = tc
 				tc.text = '1'
 				tc.font = 0x63500000
 				tc.style = 14
+				tc.outColor = 0x1
 				tp = tc
 				tp.c_mintextquadsize = setmetatable({}, c_mintextquadsize)
 				com = tp.c_mintextquadsize
@@ -182,42 +216,66 @@ tp = tc
 				--end
 			tp = tp.parent
 			--end
-			tc = G.TextQuad()
+			tc = G.Quad()
 			tp.addChild(tc)
-			tc.name = '攻击力数值'
-			tc.left = -141.167
-			tc.right = -106.167
-			tc.bottom = -194.944
-			tc.top = -153.944
-			tc.scaleX = 1.350
-			tc.scaleY = 1.350
-			tc.text = '1'
-			tc.font = 0x63500000
-			tc.style = 14
+			tc.name = '攻击力'
+			tc.left = -216.250
+			tc.right = -91.250
+			tc.bottom = -257.250
+			tc.top = -170.250
+			tc.img = 0x560b0006
 			tp = tc
-			tp.c_mintextquadsize = setmetatable({}, c_mintextquadsize)
-			com = tp.c_mintextquadsize
-			com['minWidth'] =0.000
-			com['autowidth'] =true
+				tc = G.TextQuad()
+				tp.addChild(tc)
+				tc.name = '攻击力数值'
+				tc.left = -2.500
+				tc.right = 32.500
+				tc.bottom = 3.875
+				tc.top = 44.875
+				tc.scaleX = 1.350
+				tc.scaleY = 1.350
+				tc.text = '1'
+				tc.font = 0x63500000
+				tc.style = 14
+				tc.outColor = 0x1
+				tp = tc
+				tp.c_mintextquadsize = setmetatable({}, c_mintextquadsize)
+				com = tp.c_mintextquadsize
+				com['minWidth'] =0.000
+				com['autowidth'] =true
+				tp = tp.parent
+				--end
 			tp = tp.parent
 			--end
-			tc = G.TextQuad()
+			tc = G.Quad()
 			tp.addChild(tc)
-			tc.name = '生命值数值'
-			tc.left = 120.167
-			tc.right = 155.167
-			tc.bottom = -202.961
-			tc.top = -154.483
-			tc.scaleX = 1.350
-			tc.scaleY = 1.350
-			tc.text = '3'
-			tc.font = 0x63500000
-			tc.style = 14
+			tc.name = '生命值'
+			tc.left = 101.750
+			tc.right = 184.750
+			tc.bottom = -261.375
+			tc.top = -167.375
+			tc.img = 0x560b0008
 			tp = tc
-			tp.c_mintextquadsize = setmetatable({}, c_mintextquadsize)
-			com = tp.c_mintextquadsize
-			com['minWidth'] =0.000
-			com['autowidth'] =true
+				tc = G.TextQuad()
+				tp.addChild(tc)
+				tc.name = '生命值数值'
+				tc.left = -21.250
+				tc.right = 13.750
+				tc.bottom = -3.614
+				tc.top = 44.864
+				tc.scaleX = 1.350
+				tc.scaleY = 1.350
+				tc.text = '4'
+				tc.font = 0x63500000
+				tc.style = 14
+				tc.outColor = 0x1
+				tp = tc
+				tp.c_mintextquadsize = setmetatable({}, c_mintextquadsize)
+				com = tp.c_mintextquadsize
+				com['minWidth'] =0.000
+				com['autowidth'] =true
+				tp = tp.parent
+				--end
 			tp = tp.parent
 			--end
 		tp = tp.parent
--- script/03_ui_component/c_card_weapon.lua
+++ script/03_ui_component/c_card_weapon.lua
@@ -16,21 +16,23 @@ function t:init()
     self.原画组合 = self.obj.getChildByName('原画组合')
     self.原画 = self.原画组合.getChildByName('原画')
 
-    self.职业边框 = self.obj.getChildByName('背景')
-
     self.品质板 = self.obj.getChildByName('品质板')
     self.品质宝石 = self.品质板.getChildByName('品质宝石')
 
     self.精英板 = self.obj.getChildByName('精英板')
 
-    self.名称字符 = self.obj.getChildByName('名称字符')
-    self.描述字符 = self.obj.getChildByName('描述字符')
+    self.名称板 = self.obj.getChildByName('名称板')
+    self.名称字符 = self.名称板.getChildByName('名称字符')
+    self.职业边框 = self.obj.getChildByName('描述板')
+    self.描述字符 = self.职业边框.getChildByName('描述字符')
 
     self.属性值组合 = self.obj.getChildByName('属性值组合')
     self.费用 = self.属性值组合.getChildByName('费用')
     self.费用数值 = self.费用.getChildByName('费用数值')
-    self.攻击力数值 = self.属性值组合.getChildByName('攻击力数值')
-    self.生命值数值 = self.属性值组合.getChildByName('生命值数值')
+    self.攻击力 = self.属性值组合.getChildByName('攻击力')
+    self.攻击力数值 = self.攻击力.getChildByName('攻击力数值')
+    self.生命值 = self.属性值组合.getChildByName('生命值')
+    self.生命值数值 = self.生命值.getChildByName('生命值数值')
 
     self.set_cost = G.api['CardCom_SetAttr']('cost', '费用数值', 'text')
     self.set_atk = G.api['CardCom_SetAttr']('atk', '攻击力数值', 'text')
